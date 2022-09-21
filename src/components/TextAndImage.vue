7a7af219dfb7598ebd185458cc1b116906988602
Ljubomir Davitkov <ljubomir.davitkov@students.finki.ukim.mk>
2022-09-21 18:19:18 +0200

    added component TextAndImage

---
+++ src/components/TextAndImage.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div class="text-and-image">
+      <img class="img-fluid" src="../assets/images/ljupce.jpg">
+      <div class="tekst col">
+        <h1 style="font-weight: bold; font-family: 'Padauk', sans-serif; ">Ljubomir Davitkov</h1>
+        <p style="font-family: 'Noto Serif', serif; padding: 20px;">I am a software engineer student who likes to
+          upgrade himself constantly with new knowledge and experiences. I would describe myself as a persistent and
+          ambitious person because whenever I want something I am going to achieve it. I see myself as a successful
+          software engineer in the future.</p>
+      </div>
+    </div>
+
+    <div class="text-and-image-mobile">
+      <img class="img-fluid" src="../assets/images/ljupce.jpg">
+      <div class="tekst-mobile col-mobile">
+        <h1 style="font-weight: bold;">Ljubomir Davitkov</h1>
+        <p style="font-family: 'Noto Serif', serif;">I am a software engineer student who likes to upgrade himself
+          constantly with new knowledge and experiences. I would describe myself as a persistent and ambitious person
+          because whenever I want something I am going to achieve it. I see myself as a successful software engineer
+          in the future.</p>
+      </div>
+    </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TextAndImage"
+}
+</script>
+
+<style scoped>
+
+</style>
