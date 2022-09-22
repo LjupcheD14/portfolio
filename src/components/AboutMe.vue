81c56698148a3690e390f36645218421feb2798e
Ljubomir Davitkov <ljubomir.davitkov@students.finki.ukim.mk>
2022-09-22 15:06:18 +0200

    Added data for the last list

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -175,10 +175,7 @@
               <path
                   d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
             </svg>
-            <h3 class="fw-bold mb-0 fs-4 fontfam" v-for="element in EducationTitle" :key="element">
-              {{
-                element.head_title
-              }}</h3>
+            <h3 class="fw-bold mb-0 fs-4 fontfam">Education</h3>
           </div>
           <p>
           <ul class="personal-ul" v-for="element in Education" :key="element">
@@ -203,10 +200,8 @@
               </ul>
             </li>
           </ul>
-
           </p>
         </div>
-
 
         <div class="col d-flex align-items-start">
           <div class="d-flex align-items-center">
@@ -222,69 +217,42 @@
             <p>
             <ul class="personal-ul">
 
-              <li>
-                <a>Communicative</a>
-              </li>
-
-              <li>
-                <a>Initiative</a>
-              </li>
-
-              <li>
-                <a>Team player</a>
-              </li>
-
-              <li>
-                <a>Adjustable</a>
+              <li v-for="skill in BasicSkills" :key="skill">
+                <a>{{ skill }}</a>
               </li>
 
               <li>
                 <a style="font-weight: bold">Programming</a>
               </li>
-              <li>
+              <li v-for="skill in ProgrammingSkills" :key="skill">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot"
                      viewBox="0 0 16 16">
                   <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
                 </svg>
-                <a>C, C++, C#, Java, SQL</a>
-              </li>
-              <li>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot"
-                     viewBox="0 0 16 16">
-                  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-                </svg>
-                <a>Object-oriented concept</a>
+                <a>{{ skill }}</a>
               </li>
 
               <li>
                 <a style="font-weight: bold">Web development</a>
               </li>
-              <li>
+              <li v-for="skill in WebSkills" :key="skill">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot"
                      viewBox="0 0 16 16">
                   <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
                 </svg>
-                <a>HTML, CSS, Javascript, Vue, Git</a>
+                <a>{{ skill }}</a>
               </li>
 
               <li>
                 <a style="font-weight: bold">Languages</a>
               </li>
-              <li>
+              <li v-for="element in Languages" :key="element">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot"
                      viewBox="0 0 16 16">
                   <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
                 </svg>
-                <a>English (fluent)</a>
+                <a>{{ element }}</a>
               </li>
-              <li>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot"
-                     viewBox="0 0 16 16">
-                  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-                </svg>
-                <a>Macedonian (native)</a>
-              </li>
-
             </ul>
 
             </p>
@@ -306,7 +274,7 @@ export default {
           name_surname: "Ljubomir Davitkov",
           date_birth: "14.08.2002",
           home_address: "Pitu Guli 7/a, Kochani, Macedonia",
-          tel_number: "+389 75 208 886",
+          tel_number: "Mobile number",
           email: "Email",
           linkedin: "LinkedIn",
           github: "GitHub",
@@ -331,11 +299,6 @@ export default {
         'Member of non-governmental organizations',
         'Youth international exchanges'
       ],
-      EducationTitle: [
-        {
-          head_title: "Education"
-        }
-      ],
       Education: [
         {
           title: "University",
@@ -348,7 +311,10 @@ export default {
           date: "2017 - 2021"
         }
       ],
-      Skills: ['Communicative', 'Initiative', 'Team player', 'Adjustable']
+      BasicSkills: ['Communicative', 'Initiative', 'Team player', 'Adjustable'],
+      ProgrammingSkills: ['C, C++, C#, Java, SQL', 'Object-oriented concept'],
+      WebSkills: ['HTML, CSS, Javascript, Vue, Git'],
+      Languages: ['English (fluent)', 'Macedonian (native)']
     }
   }
 }
